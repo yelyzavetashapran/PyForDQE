bc48a6b83fbf4e7959091c4e3973a115023a65c3
Liza <lizashapran13@gmail.com>
2022-11-03 16:48:22 +0200

    Home task 4, version: 2.0

--- Home_task_4(2).py
+++ Home_task_4(2).py
@@ -1,38 +1,46 @@
 from collections import defaultdict
 import random, string
 
+
 # Task 1
-def dicts():
-    output = []  # Dicts list
+def create_list_of_dicts():
+    output_1 = []  # Dicts list
     for dict_num in range(random.randint(2, 10)):  # Dicts counter
         dictionary = {}
         for elem_num in range(random.randint(3, 7)):  # Elements counter
             dictionary[random.choice(string.ascii_lowercase)] = random.randint(0, 100)  # Key(random letter), value(random number from 0 to 100)
-        output.append(dictionary)  # Append every dict to dicts list
-    return output
+        output_1.append(dictionary)  # Append every dict to dicts list
+    return output_1
+
+
+dicts = create_list_of_dicts()
+
 
 # Task 2
-output = defaultdict(lambda: float('-inf'))  # To ensure you always get the
-                                             # correct output to include if there are negative
-                                             # values use float('-inf') as the default value
-dicts = dicts()
-
-def two():
-    for cntr in range(0, len(dicts)):  # Create a counter for later adding of '_number_of_dictionary' for the same keys
-        for k, v in dicts[cntr].items():
-            if output[k] != float('-inf'):  # Check whether value exists previously
-                if output[k] > v:  # If stored value > actual than we do nothing
+def new_collective_dict_with_n_suffix(n):
+    output_2 = defaultdict(lambda: float('-inf'))  # To ensure you always get the
+    # correct output to include if there are negative
+    # values use float('-inf') as the default value
+    for cntr in range(0, len(n)):  # Create a counter for later adding of '_number_of_dictionary' for the same keys
+        for k, v in n[cntr].items():
+            if output_2[k] != float('-inf'):  # Check whether value exists previously
+                if output_2[k] > v:  # If stored value > actual than we do nothing
                     continue
-                del output[k]  # Delete key with max value
+                del output_2[k]  # Delete key with max value
                 k = f'{k}_{cntr + 1}'  # Create new key with adding '_number_of_dictionary'
 
-            output[k] = max(output[k], v)  # Add key: value
-    return output
+            output_2[k] = max(output_2[k], v)  # Add key: value
+    return output_2
 
-def three():
-    for i in range(0, len(dicts)):
-        print(f'Dict {i+1}:  {dicts[i]}')  # Just for better display
 
-three()
+new_dict = dict(new_collective_dict_with_n_suffix(dicts))
+
+
+def better_displaying(n):
+    for i in range(0, len(n)):
+        print(f'Dict {i+1}:  {n[i]}')  # Just for better display
+
+
+better_displaying(dicts)
 print('')
-print('New dictionary:', dict(two()))
+print('New dictionary:', new_dict)
--- Home_task_4(3).py
+++ Home_task_4(3).py
@@ -7,20 +7,15 @@ You NEED TO normalize it fROM letter CASEs point oF View.also, create one MORE s
 it iZ misspeLLing here.fix“iZ” with correct “ is ”, but ONLY when it Iz a mistAKE.
 
 last iz TO calculate nuMber OF Whitespace characteRS in this Text.caREFULL, not only Spaces, but ALL whitespaces.I got 87."""
-def one():
+
+
+def normalization():
     output = ''
-    last_words = []
     for i in a.lower().split('\n'):  # Convert string into lower case and split it by '\n'
         for y in i.split('.'):  # Split again by '.'
             if y.find('iz') != -1:  # If 'iz' not False
 
                 y = y.replace(' iz ', ' is ')  # Replace 'iz' with 'is' add space for replacement only where it's needed
-
-            last_word = [i.replace(':', '') for i in y.split(' ') if i != '' and len(
-                y) > 0 and not i.isdigit()]  # Create lists with every word (but not number) of every sentence, including empty (when '/n')
-
-            if len(last_word) > 0:  # If list not empty
-                last_words.append(last_word[-1])  # Append last word from each sentence to list
 
             # Creating new punctuation marks
             if y == '':
@@ -30,21 +25,39 @@ def one():
             elif y[-1].isdigit():
                 output += y.capitalize() + '.'  # For sentence 'I got 87'
             elif y[-1] == ':':
-                output += y.capitalize() + '\n'  # For sentence 'Homework'
+                output += y.capitalize() + '\n'  # For sentence 'Homework'        last_words_sentence: str = ' '.join(last_words) + '.'  # Create a new sentence(string) from list 'last_words'
 
-        last_words_sentance: str = ' '.join(last_words) + '.'  # Create a new sentence(string) from list 'last_words'
+    return output
 
-    return output, last_words_sentance
 
-output, last_words_sentance = one()
+output = normalization()
 
-def two():
-     whitespaces = output.count('\n') + output.count(' ')
-     return whitespaces
 
-def three():
-     result = output[:output.find('paragraph.')+10] + '\n' + last_words_sentance.capitalize() + output[output.find('paragraph.')+10:]  # Final output
-     return result
+def creation_of_last_words_sentence(n):
+    last_words = []
+    for i in n.lower().split('\n'):  # Convert string into lower case and split it by '\n'
+        for y in i.split('.'):  # Split again by '.'
+            last_word = [i.replace(':', '') for i in y.split(' ') if i != '' and len(
+                y) > 0 and not i.isdigit()]  # Create lists with every word (but not number) of every sentence, including empty (when '/n')
 
-print(three())
-print('Count of whitespaces:', two())
+            if len(last_word) > 0:  # If list not empty
+                last_words.append(last_word[-1])  # Append last word from each sentence to list
+
+        last_words_sentence: str = ' '.join(last_words) + '.'  # Create a new sentence(string) from list 'last_words'
+
+    return last_words_sentence
+
+last_words_sentence = creation_of_last_words_sentence(output)
+
+def count_of_whitespaces(n):
+    whitespaces = n.count('\n') + n.count(' ')
+    return whitespaces
+
+
+def append_new_sentence(n, m):
+    result = n[:n.find('paragraph.')+10] + '\n' + m.capitalize() + n[n.find('paragraph.')+10:]  # Final output
+    return result
+
+
+print(append_new_sentence(output, last_words_sentence))
+print('Count of whitespaces:', count_of_whitespaces(output))
